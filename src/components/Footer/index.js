6df5661023dcccc94109ff09e8501fe6dff78f04
Igor <ipaiva07@hotmail.com>
2022-03-20 23:14:35 -0700

    Update NavBar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,12 @@
 import React from "react";
+
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 import {
   FooterContainer,
   FooterWrap,
@@ -7,6 +15,12 @@ import {
   FooterLinkItems,
   FooterLinkTitle,
   FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIconLink,
+  SocialIcons,
+  WebsiteRights,
 } from "./FooterElements";
 
 const Footer = () => {
@@ -24,33 +38,56 @@ const Footer = () => {
               <FooterLink to="/signin"> Terms of Service</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin"> How it Works</FooterLink>
-              <FooterLink to="/signin"> Testimonials</FooterLink>
-              <FooterLink to="/signin"> Carrers</FooterLink>
-              <FooterLink to="/signin"> Investors</FooterLink>
-              <FooterLink to="/signin"> Terms of Service</FooterLink>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/signin"> Contact</FooterLink>
+              <FooterLink to="/signin"> Support</FooterLink>
+              <FooterLink to="/signin"> Destinations</FooterLink>
+              <FooterLink to="/signin"> Sponsorships</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin"> How it Works</FooterLink>
-              <FooterLink to="/signin"> Testimonials</FooterLink>
-              <FooterLink to="/signin"> Carrers</FooterLink>
-              <FooterLink to="/signin"> Investors</FooterLink>
-              <FooterLink to="/signin"> Terms of Service</FooterLink>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/signin"> Submit Video</FooterLink>
+              <FooterLink to="/signin"> Ambassadors</FooterLink>
+              <FooterLink to="/signin"> Agency</FooterLink>
+              <FooterLink to="/signin"> Influencer</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin"> How it Works</FooterLink>
-              <FooterLink to="/signin"> Testimonials</FooterLink>
-              <FooterLink to="/signin"> Carrers</FooterLink>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin"> Instagram</FooterLink>
+              <FooterLink to="/signin"> Facebook</FooterLink>
+              <FooterLink to="/signin"> Youtube</FooterLink>
               <FooterLink to="/signin"> Investors</FooterLink>
               <FooterLink to="/signin"> Terms of Service</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">MyBank</SocialLogo>
+            <WebsiteRights>
+              MyBank © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
